193e0b96ddfa073bbdce0b9f5819b8bf6156441c
Rocio Arias Ocón <rao24gr034@student.esgerencia.com>
2025-02-04 18:54:47 +0100

    Función para eliminar nombres

--- script.js
+++ script.js
@@ -12,16 +12,25 @@ function GuardarNombre() {
         document.getElementById('mensaje').innerHTML = "Introduce un nombre válido";
     }
 }
+
+//Función para eliminar el nombre del array
+function EliminarNombre(indice) {
+    nombres.splice(indice, 1);
+    CrearTabla(); 
+}
+
 // Función para crear la tabla
 function CrearTabla() {
     //hice una tabla temporal de mientras el encargado del css hacia lo suyo, para ir viendo mas o menos como funionaría
     var TablaDatos = "<table border='1'>";
-    TablaDatos += "<tr><th>Lista de Alumnos</th></tr>";
+    TablaDatos += "<tr><th>Lista de Alumnos</th><th>Eliminar</th></tr>";
 
     for (var i = 0; i < nombres.length; i++) { // Bucle para recorrer el array
         //Raya, aqui puse una clase para que en el css se ponga el pointer que se exige en el documento
         //Cambiala si quieres, modificala a tu gusto
-        TablaDatos += "<tr class='FilaTabla'><td>" + nombres[i] + "</td></tr>"; 
+        TablaDatos += "<tr class='FilaTabla'><td>" + nombres[i] + "</td>";
+        //Aqui puse el boton para eliminar, por si tambien lo quieres cambiar
+        TablaDatos += '<td><input type="button" value="Eliminar" onclick="EliminarNombre(' + i + ')"></td></tr>'; 
     }
 
     TablaDatos += "</table>";
